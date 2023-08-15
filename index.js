6804d71cb9817dc9b721436227f28a219aff1423
David Silva <david.silva@storicard.com>
2023-08-15 11:28:35 -0600

    improving error messages

--- index.js
+++ index.js
@@ -45,21 +45,21 @@ try {
 
             if (statusFound !== statusMatch) {
                 success = false
-                
+                core.setFailed(`Status must be "${statusMatch}". Found "${statusFound}". We can't continue.`);
             } else {
                 console.log(`Status of the ticket "${issueNumber}" is "${statusFound}", it is ok to continue.`);
             }
 
             if (issue.fields.fixVersions[0] == null){
                 success = false
-                core.setFailed(`FixVersions is not set in the Jira ticket ${issueNumber}`)
+                core.setFailed(`FixVersions is not set in the Jira ticket ${issueNumber}. We can't continue.`)
             } else {
                 const fixVersionsFound = issue.fields.fixVersions[0].name;
                 const formattedFixVersions = fixVersionsFound.replace(fixVersionsPrefix, targetBranchPrefix)
                 
                 if (targetBranch !== formattedFixVersions) {
                     success = false
-                    core.setFailed(`Incorrect or empty Fix Versions found in the ticket! Current target branch is "${targetBranch}". Found this was intended for "${fixVersionsFound}". We can't continue.`);
+                    core.setFailed(`Incorrect FixVersions found in the ticket! Current target branch is "${targetBranch}". Found this was intended for "${fixVersionsFound}". We can't continue.`);
                 } else {
                     console.log(`The ticket targets the appropriate release branch "${targetBranch}", with FixVersions "${fixVersionsFound}", it is ok to continue.`);
                 }
